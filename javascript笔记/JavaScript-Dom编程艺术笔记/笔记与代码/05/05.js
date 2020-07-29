521cefbb96d257f8937d6cc73de7f6e25e635a9d
lois-XLY <xuleyi980912@163.com>
2020-07-29 17:39:53 +0800

    笔记更新
    2020-7-29

--- javascript笔记/JavaScript-Dom编程艺术笔记/笔记与代码/05/05.js
+++ javascript笔记/JavaScript-Dom编程艺术笔记/笔记与代码/05/05.js
@@ -5,14 +5,58 @@ function positionMessage(){
     elem.style.position = 'absolute';
     elem.style.left = '50px';
     elem.style.top = '100px';
-    movement = setTimeout('moveMessage()',5000);
+    moveMessage("message",125,25,20);
 }
 
-function moveMessage(){
+// function moveMessage(){
+//     if(!document.getElementById) return false;
+//     if(!document.getElementById('message')) return false;
+//     var elem = document.getElementById('message');
+//     var xpos = parseInt(elem.style.left);//获取当前距离左侧的距离，由于属性带单位所以要转换类型
+//     var ypos = parseInt(elem.style.top);//距顶部距离
+//     if(xpos === 200 && ypos === 100) return true;//判断是否已经到达目标点
+//     if(xpos < 200){
+//         xpos++;
+//     }
+//     if(xpos > 200){
+//         xpos--;
+//     }
+//     if(ypos < 100){
+//         ypos++;
+//     }
+//     if(ypos > 100){
+//         y--;
+//     }
+//     elem.style.left = xpos + 'px';
+//     elem.style.top = ypos + 'px';
+//     movement = setTimeout('moveMessage()',10);
+// }
+
+//抽象
+
+function moveMessage(elementID,final_x,final_y,interval){
     if(!document.getElementById) return false;
-    if(!document.getElementById('message')) return false;
-    var elem = document.getElementById('message');
-    elem.style.left = '200px';
+    if(!document.getElementById(elementID)) return false;
+    var elem = document.getElementById(elementID);
+    var xpos = parseInt(elem.style.left);//获取当前距离左侧的距离，由于属性带单位所以要转换类型
+    var ypos = parseInt(elem.style.top);//距顶部距离
+    if(xpos === final_x && ypos === final_y) return true;//判断是否已经到达目标点
+    if(xpos < final_x){
+        xpos++;
+    }
+    if(xpos > final_x){
+        xpos--;
+    }
+    if(ypos < final_y){
+        ypos++;
+    }
+    if(ypos > final_y){
+        ypos--;
+    }
+    elem.style.left = xpos + 'px';
+    elem.style.top = ypos + 'px';
+    var repeat = "moveMessage('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+    movement = setTimeout(repeat,10);
 }
 
 function addLoadEvent(func) {//页面加载时可同时完成多个函数（可复用）
