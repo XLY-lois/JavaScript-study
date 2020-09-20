feb6d4d20dd969fe87ce12e177a90be10de89d44
XLY-lois <xuleyi980912@163.com>
2020-09-20 21:35:37 +0800

    es6

---
+++ javascript笔记/ES6/笔记/08.js
@@ -0,0 +1,15 @@
+
+let name = 'lois'
+let skill = '发呆'
+//es5 对象的赋值
+// let obj = {
+//     name:name,
+//     skill:skill
+// }
+
+//es6 对象的赋值
+let obj = {name,skill};
+console.log(obj);//{name: "lois", skill: "发呆"}
+
+//key值的构建
+let key = 'skill';
